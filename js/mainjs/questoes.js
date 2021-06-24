905a6b95be2a299c4bd70c8b1b6368c150884f44
Kevin Narita <kevin.narita@outlook.com>
2021-06-24 06:15:32 -0300

    ajustes nas tabelas

--- js/mainjs/questoes.js
+++ js/mainjs/questoes.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
             "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
 
         });
+        $("#deletarQuestao").prop("disabled", true)
 
 
 
@@ -50,13 +51,116 @@ $(document).ready(function () {
     })
     //!
     //! Opção das alternativas
-    $(".toggleAlternativa").click(function () {
+    $("#cadastrarQuestao").on("click", ".toggleAlternativa", function () {
         $(this).text() == "Incorreta" ? $(this).text("Correta").removeClass("btn-danger").addClass("btn-success") :
             $(this).text("Incorreta").removeClass("btn-success").addClass("btn-danger");
-    })
+
+    });
     //!
     $('#tableQuestoes').on('mouseenter', 'tbody tr', function () {
         var rowData = tableQuestoes.row(this).data();
 
     });
+
+    i = 0;
+    $(".form-group").on('click', "#adicionarQuestao", function () {
+        letra = ["A)", "B)", "C)", "D)", "E)", "F)", "G)", "H)", "I)", "J)"];
+        //A=0,B=1,C=2,D=3,E=4,F=5,G=6,H=7,I=8,J=9
+        i < 0 ? $("#deletarQuestao").prop("disabled", true) : $("#deletarQuestao").prop("disabled", false);
+        escolherSwitch(letra, i)
+        i++;
+
+    });
+
+    $(".form-group").on('click', "#deletarQuestao", function () {
+        $('#alternativas').each(function () {
+            let index = $(this).find(".list-group-item").last().attr("id")
+            i = parseInt(index);
+            $(this).find(".list-group-item").last().remove();
+        });
+        i == 0 ? $("#deletarQuestao").prop("disabled", true) : $("#deletarQuestao").prop("disabled", false);
+    });
+
+    function escolherSwitch(letra, i) {
+        switch (i) {
+            case 0: {
+                carregarAlternativa(letra[0], i)
+            }
+                break;
+            case 1: {
+                carregarAlternativa(letra[1], i)
+            }
+                break;
+            case 2: {
+                carregarAlternativa(letra[2], i);
+            }
+                break;
+            case 3: {
+                carregarAlternativa(letra[3], i);
+            }
+                break;
+            case 4: {
+                carregarAlternativa(letra[4], i);
+            }
+                break;
+            case 5: {
+                carregarAlternativa(letra[5], i);
+            }
+                break;
+            case 6: {
+                carregarAlternativa(letra[6], i);
+            }
+                break;
+            case 7: {
+                carregarAlternativa(letra[7], i);
+            }
+                break;
+            case 8: {
+                carregarAlternativa(letra[8], i);
+            }
+                break;
+            case 9: {
+                carregarAlternativa(letra[9], i);
+            }
+                break;
+        }
+
+        function carregarAlternativa(data, i) {
+
+            let letra = data;
+            let letraid = data;
+
+            letraid = letraid.replace(')', '')
+
+            let carregar = `    <li  id="${i}" class="list-group-item">
+                                <div class="form-group">
+                                <div class="input-group">               
+                                    <div id="botao-alternativa" class="input-group-prepend">
+                                        <button type="button" class="btn btn-danger toggleAlternativa">Incorreta</button>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text bg-primary ">${letra}</span>
+                                    </div>
+                                    <textarea class="form-control" id="alternativa${letraid}" rows="3"></textarea>
+                                </div>
+                            </div>
+                        </li>`
+
+            $("#alternativas").append(carregar);
+        }
+    }
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
