869a5bc3ea2c138ec322fcd4247efc77feecd7af
Kevin Narita <kevin.narita@outlook.com>
2021-09-11 06:08:32 -0300

    Adicionado Ajax no formluario

--- js/mainjs/questoes.js
+++ js/mainjs/questoes.js
@@ -25,13 +25,38 @@ $(document).ready(function () {
                 { data: 'quePalavrasChave' },
                 { data: 'subDescricao' },
                 { data: 'queCodigoBncc' },
-                { data: 'queNivel' },
-                { data: 'queAnoID' },
-                { data: 'queStsTipo' },
-                { data: 'queStsRevisao' },
                 {
                     data: null, render: function (data, type, row) {
-
+                        if (data.nivDescricao == "Fácil") {
+                            return `<label class="badge badge-success">${data.nivDescricao}</label>`;
+                        } else if (data.nivDescricao == "Intermediário") {
+                            return `<label class="badge badge-warning">${data.nivDescricao}</label>`;
+                        } else {
+                            return `<label class="badge badge-danger">${data.nivDescricao}</label>`;
+                        }
+                    }
+                },
+                { data: 'queAnoID' },
+                {
+                    data: null, render: function (data, type, row) {
+                        if (data.queStsTipo == "Publica") {
+                            return `<label class="badge badge-info">${data.queStsTipo}</label>`;
+                        } else {
+                            return `<label class="badge badge-warning">${data.queStsTipo}</label>`;
+                        }
+                    }
+                },
+                {
+                    data: null, render: function (data, type, row) {
+                        if (data.queStsRevisao == "Revisada") {
+                            return `<label class="badge badge-success">${data.queStsRevisao}</label>`;
+                        } else {
+                            return `<label class="badge badge-danger">${data.queStsRevisao}</label>`;
+                        }
+                    }
+                },
+                {
+                    data: null, render: function (data, type, row) {
                         return ` <button type="button"
                                     class="btn btn-inverse-primary btn-rounded btn-icon btn-info-questao">
                                     <i class="bi bi-info-lg"></i>
@@ -55,6 +80,81 @@ $(document).ready(function () {
 
 
     }
+    //! Formulário de cadastro de Questões
+    $('#formQuestoes').submit(function (e) {
+        e.preventDefault();//evita de dar reload na pagina
+        var subgrupoopc = $('#subgrupoopc').val();
+        var nivelopc = $('#nivelopc').val();
+        var codigobncc = $("#codigobncc").val();
+        var enunciado = $("#enunciado").val();
+        var palavrasChave = $("#palavrasChave").val();
+        var data = {
+            subgrupoopc: subgrupoopc,
+            nivelopc: nivelopc,
+            codigobncc: codigobncc,
+            enunciado: enunciado,
+            palavrasChave: palavrasChave
+        };
+
+        $('li textarea').each(function () {
+
+            let alternativa = $(this).attr("id"); //* Pega o id da alternativa
+            let textoAlternativa = $(this).val();//* Pega o enuncado da alternativa
+            let status = $(this).closest("div").children().children(".toggleAlternativa").text();//* Pega o status de Correta ou Incorreta da alternativa
+
+            data[alternativa] = alternativa //? Insere um novo valor no objeto alternativaA : alternativaA
+
+            alternativa = alternativa.concat("texto");//* Muda o nome alternativaA para alternativaAtexto
+            data[alternativa] = textoAlternativa//? Insere um novo valor no objeto AlternativaAtexto : textoAlternativa
+
+            alternativa = alternativa.replace("texto", "status");//* Muda o nome alternativaAtexto para alternativaAstatus
+            data[alternativa] = status//? Insere um novo valor no objeto AlternativaAstatus : status
+            // Object.assign(data, { keys[i]: alternativa });
+
+        });
+        //console.log("Objeto data: ", data)
+
+        $.ajax({
+            url: '../backend/questoesBack.php',
+            method: 'POST',
+            data: data,
+            dataType: 'json',
+            success: function (data) {
+                if (data.type == 'erro') {
+                    Swal.fire({
+                        position: "center",
+                        icon: "error",
+                        title: data.text,
+                        showConfirmButton: false,
+                        timer: 2000
+                    })
+
+                } else if (data.type == 'sucesso') {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: data.text,
+                        showConfirmButton: false,
+                        timer: 2000
+                    })
+
+                    $("#btn-nova-disciplina").click();//Simula um click manual no botao de cadastrar
+                } else if (data.type == 'validacao') {
+                    Swal.fire({
+                        position: "center",
+                        icon: "warning",
+                        title: data.text,
+                        showConfirmButton: false,
+                        timer: 2000
+                    })
+                }
+            }
+        }).done(function (data) {
+            tabelaQuestoes.ajax.reload(null, false);
+        });
+
+        return false;
+    });
 
     function toggleNovaQuestao() {
         let adicionarIcon = `<i class="bi bi-plus-circle btn-icon-prepend"></i>`;
@@ -96,17 +196,16 @@ $(document).ready(function () {
 
 
 
-
+    //! Botão de adicionar alternativas
     i = 0;
     $(".form-group").on('click', "#adicionarQuestao", function () {
-        letra = ["A)", "B)", "C)", "D)", "E)", "F)", "G)", "H)", "I)", "J)"];
-        //A=0,B=1,C=2,D=3,E=4,F=5,G=6,H=7,I=8,J=9
+        letra = ["A)", "B)", "C)", "D)", "E)", "F)", "G)", "H)", "I)", "J)", "K)", "L)", "M)", "N)", "O)", "P)"];
+        //A=0,B=1,C=2,D=3,E=4,F=5,G=6,H=7,I=8,J=9,K=10,L=11,M=12,N=13,O=14,P=15
         i < 0 ? $("#deletarQuestao").prop("disabled", true) : $("#deletarQuestao").prop("disabled", false);
         escolherSwitch(letra, i)
         i++;
-
     });
-
+    //! Botão de remover alternativas
     $(".form-group").on('click', "#deletarQuestao", function () {
         $('#alternativas').each(function () {
             let index = $(this).find(".list-group-item").last().attr("id")
@@ -158,15 +257,38 @@ $(document).ready(function () {
                 carregarAlternativa(letra[9], i);
             }
                 break;
+            case 10: {
+                carregarAlternativa(letra[10], i);
+            }
+                break;
+            case 11: {
+                carregarAlternativa(letra[11], i);
+            }
+                break;
+            case 12: {
+                carregarAlternativa(letra[12], i);
+            }
+                break;
+            case 13: {
+                carregarAlternativa(letra[13], i);
+            }
+                break;
+            case 14: {
+                carregarAlternativa(letra[14], i);
+            }
+                break;
+            case 15: {
+                carregarAlternativa(letra[15], i);
+            }
+                break;
         }
 
         function carregarAlternativa(data, i) {
-
             let letra = data;
             let letraid = data;
-
+            console.log(i);
             letraid = letraid.replace(')', '')
-
+            console.log(letraid);
             let carregar = `    <li  id="${i}" class="list-group-item">
                                 <div class="form-group">
                                 <div class="input-group">               
@@ -176,7 +298,7 @@ $(document).ready(function () {
                                     <div class="input-group-append">
                                         <span class="input-group-text bg-primary ">${letra}</span>
                                     </div>
-                                    <textarea class="form-control" id="alternativa${letraid}" rows="3"></textarea>
+                                    <textarea class="form-control" id="alternativa${letraid}" name="alternativa${letraid}"rows="3"></textarea>
                                 </div>
                             </div>
                         </li>`
@@ -184,8 +306,25 @@ $(document).ready(function () {
             $("#alternativas").append(carregar);
         }
     }
+    //! Select de subgrupo
+    $("#cadastrarQuestao").on("change", "#subgrupoopc", function () {
+        let opSelecionada = $(this).val();
+        $.ajax({
+            url: '../backend/questoesBack.php',
+            method: 'POST',
+            data: {
+                opSelecionada: opSelecionada,
+            },
+            dataType: 'json',
+            success: function (data) {
+                let disciplina = `<label class="labelCadastroAtuacao">Temática</label>  <div class="btn btn-inverse-primary btn-fw" style="cursor: default">${data.disDescricao}</div>`
+                let tematica = `<label class="labelCadastroAtuacao">Disciplina</label>  <div class="btn btn-inverse-primary btn-fw" style="cursor: default">${data.temDescricao}</div>`
+                $("#temSelecionado").html(tematica);
+                $("#disSelecionado").html(disciplina);
+            }
 
-
+        })
+    })
 
 
 
