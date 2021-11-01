3bce6d23178f7e3f48808b6e14f190f607f4a2af
Kevin Narita <kevin.narita@outlook.com>
2021-11-01 02:26:53 -0300

    Corrigido erro de português

--- pages/questoes.php
+++ pages/questoes.php
@@ -319,7 +319,7 @@ $n = new Nivel("pedagogy", "localhost", "root", "");
                         <div class="modal-content">
                             <form id="formDelete">
                                 <div class="modal-header">
-                                    <h4 class="modal-title ml-auto">Deseja excluir o item selecionada?</h4>
+                                    <h4 class="modal-title ml-auto">Deseja excluir o item selecionado?</h4>
                                     <button type="button" id="buttonXmodal" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
@@ -328,10 +328,9 @@ $n = new Nivel("pedagogy", "localhost", "root", "");
                                 </div>
 
                                 <div class="modal-footer">
-                                    <button id="modalCancelar" type="button" class="btn btn-secondary  mr-auto">Cancelar</button>
+                                    <button id="modalCancelar" type="button" class="btn btn-secondary  mr-auto"><i class="bi bi-x-circle btn-icon-prepend "></i> Cancelar</button>
                                     <button id="modalConfirmar" type="submit" class="btn btn-primary">
-                                        <i class="bi bi-x-circle btn-icon-prepend "></i>
-                                        Excluir</button>
+                                        <i class="bi bi-check2-circle btn-icon-prepend "></i> Excluir</button>
                                 </div>
                             </form>
                         </div>
