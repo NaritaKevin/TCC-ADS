7f1bac48d6563485b6212bbf7b388d7761f24a9b
Kevin Narita <kevin.narita@outlook.com>
2021-11-16 02:24:33 -0300

    layout do menu

---
+++ js/mainjs/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $("#cardAtividade").click(function () {
+        window.location.href = './pages/atividades.php';
+    })
+    $("#cardQuestao").click(function () {
+        window.location.href = './pages/questoes.php';
+    })
+    $("#cardDisciplina").click(function () {
+        window.location.href = './pages/atuacao.php';
+    })
+    $("#cardResultado").click(function () {
+        window.location.href = './pages/resultados.php';
+    })
+
+})
